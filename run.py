837c7b08bac755108141444e916235598a7f9192
JoshuaCarroll1 <joshcarroll8457@gmail.com>
2023-03-31 13:40:54 +0000

    Amend opening logo and add game setup

--- run.py
+++ run.py
@@ -10,20 +10,20 @@ def opening_logo():
     Logo for opening of game 
     """ 
     print(Back.RED+ "========  ========  ========  ========  ========      ===     ==       ==") 
-    print(Back.RED+ "==        ==    ==  ==    ==     ==     ==     ==    == ==    ==       ==")
-    print(Back.RED+ "==        ==    ==  ==    ==     ==     ==     ===  ==   ==   ==       ==")
-    print(Back.RED+ "==        ==    ==  ==    ==     ==     ==     ==   ==   ==   ==       ==")
-    print(Back.RED+ "========  ==    ==  ==    ==     ==     ========    =======   ==       ==")
-    print(Back.RED+ "==        ==    ==  ==    ==     ==     ==     ==   ==   ==   ==       ==")
-    print(Back.RED+ "==        ==    ==  ==    ==     ==     ==      ==  ==   ==   ==       ==")
-    print(Back.RED+ "==        ==    ==  ==    ==     ==     ==     ==   ==   ==   ==       ==")
+    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==    || ||    ||       ||")
+    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ===  ||   ||   ||       ||")
+    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")
+    print(Back.RED+ "========  ||    ||  ||    ||     ||     ========    =======   ||       ||")
+    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")
+    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||      ==  ||   ||   ||       ||")
+    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")
     print(Back.RED+ "==        ========  ========     ==     ========    ==   ==   =======  =======/n")
     print(Back.RED+ "========   ==    ==  ======  =======")
-    print(Back.RED+ "==    ==   ==    ==    ==         =")
-    print(Back.RED+ "==    ==   ==    ==    ==        =")
-    print(Back.RED+ "==    ==   ==    ==    ==       =")
-    print(Back.RED+ "==    ==   ==    ==    ==      =")
-    print(Back.RED+ "==    ==   ==    ==    ==     =")
+    print(Back.RED+ "||    ||   ||    ||    ||         ||")
+    print(Back.RED+ "||    ||   ||    ||    ||        ||")
+    print(Back.RED+ "||    ||   ||    ||    ||       ||")
+    print(Back.RED+ "||    ||   ||    ||    ||      ||")
+    print(Back.RED+ "||    ||   ||    ||    ||     ||")
     print(Back.RED+ "=========  ========  ======  =======/n")   
 
 
@@ -33,10 +33,13 @@ def clear():
     """
     os.system("cls" if os.name == "nt" else "clear")
 
+
 def game_setup():
     """
     Game start up prior to enetering name.
     """
+    print(Fore.GREEN+ "Welcome to the Football Quiz!")
+    print(Fore.GREEN+ "Please add your username below!")
 
 
 from google.oauth2.service_account import Credentials
