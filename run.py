a9420092e5b1c2c0e1e9ba7c5089bdcc581b1374
JoshuaCarroll1 <joshcarroll8457@gmail.com>
2023-04-07 14:02:43 +0000

    Refactor code for pep8

--- run.py
+++ run.py
@@ -2,84 +2,12 @@ import gspread
 import os
 import time
 import random
-from colors import colorama
-from questions import QUESTIONS
-
-
-def opening_logo():
-    """
-    Logo for opening of game 
-    """ 
-    print(Back.RED+ "========  ========  ========  ========  ========      ===     ==       ==") 
-    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==    || ||    ||       ||")
-    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ===  ||   ||   ||       ||")
-    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")
-    print(Back.RED+ "========  ||    ||  ||    ||     ||     ========    =======   ||       ||")
-    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")
-    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||      ==  ||   ||   ||       ||")
-    print(Back.RED+ "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")
-    print(Back.RED+ "==        ========  ========     ==     ========    ==   ==   =======  =======/n")
-    print(Back.RED+ "========   ==    ==  ======  =======")
-    print(Back.RED+ "||    ||   ||    ||    ||         ||")
-    print(Back.RED+ "||    ||   ||    ||    ||        ||")
-    print(Back.RED+ "||    ||   ||    ||    ||       ||")
-    print(Back.RED+ "||    ||   ||    ||    ||      ||")
-    print(Back.RED+ "||    ||   ||    ||    ||     ||")
-    print(Back.RED+ "=========  ========  ======  =======/n")   
-
-
-def clear():
-    """
-    Clear function to make terminal look cleaner.
-    """
-    os.system("cls" if os.name == "nt" else "clear")
-
-
-def game_setup():
-    """
-    Game start up prior to entering name.
-    """
-    print(Fore.GREEN+ "Welcome to the Football Quiz!")
-    print(Fore.GREEN+ "Please add your username below!")
-
-
-def username_setup():
-    """
-    Add username here.
-    """
-    username = input("Type name here and click return: ")
-    print(Fore.GREEN+ "Hello" + username + ", lets test your ball knowledge!")
-    
-
-def validator_username(username_setup):
-    """
-    Used to validate name has under a certain amount of characters and only uses alphabet
-    """    
-    if username_setup == '':
-        print(Fore.RED+ "No usersname given, please try again/n")
-        return False
-    elif len(username_setup) > 15:
-        print(Fore.RED+ "Usersname given is too long, please try again/n")
-        return False
-    elif len(username_setup) < 4:
-        print(Fore.RED+ "Usersname given is too short, please try again/n")
-        return False
-    elif username_setup.isalpha():
-        print(Fore.RED+ "Usersname given cannot contain letters only!/n")
-        return False
-      
-    else:
-        return True
-
-
-def questions_validator():
-    """
-    Questions validator to show questions and answers
-    """
-    print(QUESTIONS)
-
-
+import colorama
+from colorama import Fore, Back
 from google.oauth2.service_account import Credentials
+from questions import QUESTIONS
+colorama.init(autoreset=True)
+
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -93,13 +21,90 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('football_quiz')
 
 
-def main():
+def opening_logo():
     """
-    Main function to run program 
+    Logo for opening of game
     """
-    opening_logo()
+    print(Back.RED + "========  ========  ========  ========  ========      ===     ==       ==")  # noqa
+    print(Back.RED + "||        ||    ||  ||    ||     ||     ||     ==    || ||    ||       ||")  # noqa
+    print(Back.RED + "||        ||    ||  ||    ||     ||     ||     ===  ||   ||   ||       ||")  # noqa
+    print(Back.RED + "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")  # noqa
+    print(Back.RED + "========  ||    ||  ||    ||     ||     ========    =======   ||       ||")  # noqa
+    print(Back.RED + "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")  # noqa
+    print(Back.RED + "||        ||    ||  ||    ||     ||     ||      ==  ||   ||   ||       ||")  # noqa
+    print(Back.RED + "||        ||    ||  ||    ||     ||     ||     ==   ||   ||   ||       ||")  # noqa
+    print(Back.RED + "==        ========  ========     ==     ========    ==   ==   =======  =======")  # noqa
+    print("")
+    print(Back.RED + "========   ==    ==  ======  =======")
+    print(Back.RED + "||    ||   ||    ||    ||         ||")
+    print(Back.RED + "||    ||   ||    ||    ||        ||")
+    print(Back.RED + "||    ||   ||    ||    ||       ||")
+    print(Back.RED + "||    ||   ||    ||    ||      ||")
+    print(Back.RED + "||    ||   ||    ||    ||     ||")
+    print(Back.RED + "=========  ========  ======  =======")
+
+
+def clear():
+    """
+    Clear function to make terminal look cleaner.
+    """
+    os.system("cls" if os.name == "nt" else "clear")
+
+
+def game_setup():
+    """
+    Game start up prior to entering name.
+    """
+    print(Fore.GREEN + "Welcome to the Football Quiz!")
+    print(Fore.GREEN + "Please add your username below!")
+
+
+def username_setup():
+    """
+    Add username here.
+    """
+    while True:
+        username = input("Type name here and click return: ")
+        if validator_username(username):
+            break
     clear()
+    print(Fore.GREEN + "Hello" + username + ", lets test your ball knowledge!")
+
+
+def validator_username(username):
+    """
+    Used to validate name has under a certain
+    amount of characters and only uses alphabet
+    """
+    if username == '':
+        print(Fore.RED + "No usersname given, please try again/n")
+        return False
+    elif len(username) > 15:
+        print(Fore.RED + "Usersname given is too long, please try again/n")
+        return False
+    elif len(username) < 3:
+        print(Fore.RED + "Usersname given is too short, please try again/n")
+        return False
+    elif not username.isalpha():
+        print(Fore.RED + "Usersname given cannot contain letters only!/n")
+        return False
+    else:
+        return True
+
+
+def questions_validator():
+    """
+    Questions validator to show questions and answers
+    """
+    print(QUESTIONS)
+
+
+if __name__ == "__main__":
+    """
+    Main function to run program
+    """
+    clear()
+    opening_logo()
     game_setup()
     username_setup()
     questions_validator()
-    
