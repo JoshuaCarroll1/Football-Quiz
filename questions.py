8ce8aac723b44a9f8cc7e14a629e07349f3e84f2
JoshuaCarroll1 <joshcarroll8457@gmail.com>
2023-04-07 14:02:16 +0000

    Fix broken questions dictionary

--- questions.py
+++ questions.py
@@ -1,303 +1,333 @@
 QUESTIONS = [
     {
         "question": "Who previously scored the most international goals prior to Cristiano Ronaldo?",
-        "correct answer": "B"
+        "correct_answer": "B",
         "choices": [
             "A. Lionel Messi",
             "B. Pele",
             "C. Ali Daei",
             "D. Mokhtar Dahari",
+
         ]
     },
     {
         "question": "Who has won the most Premier League titles as a manager?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Sir Alex Ferguson",
             "B. Manchester United",
             "C. Pep Guardiola",
             "D. Arsene Wenger",
+
         ]
     },
     {
         "question": "Who has scored the most Champions League goals in one season?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Lionel Messi",
             "B. Karim Benzema",
             "C. Diego Maradona",
             "D. Cristiano Ronaldo",
+
         ]
     },
     {
         "question": "Which team has won the most Champions League Trophies?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Real Madrid",
             "B. Barcalona FC",
             "C. PSG",
             "D. Liverpool FC",
+
         ]
     },
     {
         "question": "Which team has won in the Top 5 leagues has won the most domestic league titles in a row?",
-        "correct answer": "B"
+        "correct_answer": "B",
         "choices": [
             "A. Real Madrid",
             "B. Bayern Munich",
             "C. PSG",
             "D. Manchester City",
+
         ]
     },
     {
         "question": "Which club has the best transfer profit in the top 5 leagues in the last decade?",
-        "correct answer": "C"
+        "correct_answer": "C",
         "choices": [
             "A. lyon",
             "B. Sporting FC",
             "C. Benfica",
             "D. Brighton",
+
         ]
     },
     {
         "question": "Which team has won the most FA Cups Trophies?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Arsenal FC",
             "B. Manchester United",
             "C. Manchester City",
             "D. Spurs",
+
         ]
     },
     {
         "question": "Which team  won the most World Cup in 1990?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Argentina",
             "B. Brazil",
             "C. Uruguay",
             "D. Germany",
+
         ]
     },
     {
         "question": "How many times Pele won the World Cup?",
-        "correct answer": "C"
+        "correct_answer": "C",
         "choices": [
             "A. 1",
             "B. 2",
             "C. 3",
             "D. 4",
+
         ]
     },
     {
         "question": "What was the largest transfer fee for a Football player?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Neymar",
             "B. Mbappe",
             "C. Gareth Bale",
             "D. Cristiano Ronaldo",
+
         ]
     },
     {
         "question": "Which player scored the famous Hand of God goal?",
-        "correct answer": "B"
+        "correct_answer": "B",
         "choices": [
             "A. Joe Cole",
             "B. Diego Maradona",
             "C. Ashley Cole",
             "D. Luis Suarez",
+
         ]
     },
     {
         "question": "Which player has the most trophies?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Lionel Messi",
             "B. Dani Alves",
             "C. Ryan Giggs",
             "D. Harry Kane",
+
         ]
     },
     {
         "question": "Which player won the Ballon Dor prior to Lionel Messi and Cristiano Ronaldos dominance?",
-        "correct answer": "C"
+        "correct_answer": "C",
         "choices": [
             "A. Luka Modric",
             "B. Luis Figo",
             "C. Kaka",
             "D. Rio Ferdinand",
+
         ]
     },
     {
         "question": "Which player won the 1970 Ballon Dor?",
-        "correct answer": "B"
+        "correct_answer": "B",
         "choices": [
             "A. Bobby Moore",
             "B. Gerd Müller",
             "C. Pele",
             "D. George Best",
+
         ]
     },
     {
         "question": "Which goalkeeper won the Ballon Dor?",
-        "correct answer": "C"
+        "correct_answer": "C",
         "choices": [
             "A. Casillas",
             "B. Neuer",
             "C. Yashin",
             "D. Karius",
+
         ]
     },
 
     {
         "question": "Which team has Ronaldinho never played for?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Barcalona FC",
             "B. Grêmio",
             "C. PSG",
             "D. Sao Paulo FC",
+
         ]
     },
     {
         "question": "Which league is outside the Top 5 leagues?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Ligue 1",
             "B. Serie A",
             "C. Bundesliga",
             "D. Eredivisie",
+
         ]
     },
     {
         "question": "Who held the Fa Cup for the longest time?",
-        "correct answer": "B"
+        "correct_answer": "B",
         "choices": [
             "A. Arsenal",
             "B. Portsmouth",
             "C. Manchester United",
             "D. Manchester City",
+
         ]
     },
     {
         "question": "Which Player headbutt an oppostion player during a World Cup Final?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Zidane",
             "B. Diego Maradona",
             "C. Sergio Ramos",
             "D. Sergio Aguero",
+
         ]
     },
     {
         "question": "Which team beat Aregntina in the World Cup 2022?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. No one",
             "B. Mexico",
             "C. France",
             "D. Saudi Arabia",
+
         ]
     },
     {
         "question": "Which team is the most successful team in England for Major trophies?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Liverpool FC",
             "B. Manchester United FC",
             "C. CHelsea FC",
             "D. Arsenal FC",
+
         ]
     },
     {
         "question": "Which team has not won the Copa America trophies?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Argentina",
             "B. Brazil",
             "C. Uruguay",
             "D. France",
+
         ]
     },
     {
         "question": "Which player scored the goal to win Portugal their first Euros in the Final?",
-        "correct answer": "C"
+        "correct_answer": "C",
         "choices": [
             "A. Cristiano Ronaldo",
             "B. Nani",
             "C. Eder",
             "D. Quaresma",
+
         ]
     },
     {
         "question": "Which player won a Premier League , transfered to another club, and won another Premier League that next season recently?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. Ngolo Kante",
             "B. Eric Cantona",
             "C. Cesc Fabregas",
             "D. Frank Lampard",
+
         ]
     },
     {
         "question": "How many own goals did Jamie Carragher score against Liverpool in his career?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. 1",
             "B. 3",
             "C. 5",
             "D. 7",
+
         ]
     },
     {
         "question": "Which player won MVP in Serie A in the 2021/ 2022 season?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Olivier Giroud",
             "B. Zlatan Ibrahimović",
             "C. Theo Hernandez",
             "D. Rafael Leao",
+
         ]
     },
     {
         "question": "Who has scored the most goals in one calendar year?",
-        "correct answer": "B"
+        "correct_answer": "B",
         "choices": [
             "A. Neymar Jr",
             "B. Lionel Messi",
             "C. Cristiano Ronaldo",
             "D. Luis Suarez",
+
         ]
     },
     {
         "question": "Who won the first ever Puskas?",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Puskas",
             "B. Pele",
             "C. Lionel Messi",
             "D. Cristiano Ronaldo",
+
         ]
     },
     {
         "question": "How many Ballon Dors has Pele won?",
-        "correct answer": "A"
+        "correct_answer": "A",
         "choices": [
             "A. 0",
             "B. 1",
             "C. 3",
             "D. 4",
+
         ]
     },
     {
         "question": "Who scored the final penalty for Argentina to win the Wrold Cup 2022",
-        "correct answer": "D"
+        "correct_answer": "D",
         "choices": [
             "A. Lionel Messi",
             "B. Lisandro Martínez",
             "C. Rodrigo De Paul",
             "D. Gonzalo Montiel",
+
         ]
     },
 ]
